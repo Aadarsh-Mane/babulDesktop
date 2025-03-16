3c4a34b2aa5348af671ac7e9506232b25d04cf8a
Aadarsh-Mane <onlyaddy68@gmail.com>
2025-03-17 02:19:44 +0530

    Add initial project structure and configuration files for Flutter application

---
+++ lib/constants/Assets.dart
@@ -0,0 +1,24 @@
+class AppImages {
+  static const String logo = 'assets/images/bagul-logo.png';
+  static const String logoLight = 'assets/images/logo-light.png';
+  static const String admin = 'assets/images/admin-panel.png';
+  static const String reception = 'assets/images/bagul-reception.png';
+  static const String home = 'assets/images/bagul-main.png';
+}
+
+class AppStrings {
+  static const String admin = 'Admin';
+  static const String reception = 'Reception';
+  static const String hospitalName = 'Bagul Hospital';
+  void getHospitalName() {
+    print(hospitalName);
+  }
+}
+
+class AllUserPassword {
+  static const String receptionUser = 'Reception';
+  static const String receptionPassword = 'Bagul123';
+
+  static const String adminUser = 'Admin';
+  static const String adminPassword = 'Bagul123';
+}
